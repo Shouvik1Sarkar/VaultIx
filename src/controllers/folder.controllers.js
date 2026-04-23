de67e3360759006e00bc737fd64b29d7efb14051
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-04-23 14:17:38 +0530

    refactor: improve error handling and user authentication checks in folder operations; enhance response messages

--- src/controllers/folder.controllers.js
+++ src/controllers/folder.controllers.js
@@ -10,46 +10,52 @@ import asyncHandler from "../utils/asyncHandler.utils.js";
 // });
 export const createFolder = asyncHandler(async (req, res) => {
   const user = req.user;
-  const userId = req.user._id;
+
   if (!user) {
     throw new ApiError(401, "Not logged in");
   }
+
+  const userId = user._id;
   const { folderName } = req.body;
-  const folder = await Folder.create({ folderName, createdBy: user._id });
-  // console.log("FOLDER: ", folder);
+
+  const folder = await Folder.create({ folderName, createdBy: userId });
+
   if (!folder) {
-    throw new ApiError(400, "FOLDER NOT CREATED.");
+    throw new ApiError(500, "Folder not created");
   }
+
   await redisClient.del(`folder:${userId}`);
-  return res.status(201).json(new ApiResponse(201, folder, "This is it"));
+  return res.status(201).json(new ApiResponse(201, folder, "Folder created"));
 });
 
 export const deleteFolder = asyncHandler(async (req, res) => {
   const user = req.user;
-  const userId = req.user._id;
+
+  if (!user) {
+    throw new ApiError(401, "Not logged in");
+  }
+
+  const userId = user._id;
   const { folder } = req.params;
 
   const findFolder = await Folder.findOne({
     _id: folder,
-    createdBy: user._id,
+    createdBy: userId,
   });
-
-  // console.log("FIND FOLDER: ", findFolder);
 
   if (!findFolder) {
     throw new ApiError(404, "Folder not found");
   }
+
   if (["all", "video", "article", "post"].includes(findFolder.folderName)) {
     throw new ApiError(400, "System folder cannot be deleted");
   }
-  // find default folder
+
   const defaultFolder = await Folder.findOne({
     folderName: "all",
-    createdBy: user._id,
+    createdBy: userId,
   });
-  // console.log("DEFAULT FODLER: ", defaultFolder);
 
-  // move bookmarks instead of deleting
   await BookMarked.updateMany(
     { folderId: findFolder._id },
     { $set: { folderId: defaultFolder._id } },
@@ -62,13 +68,18 @@ export const deleteFolder = asyncHandler(async (req, res) => {
 
 export const renameFolder = asyncHandler(async (req, res) => {
   const user = req.user;
-  const userId = req.user._id;
+
+  if (!user) {
+    throw new ApiError(401, "Not logged in");
+  }
+
+  const userId = user._id;
   const { name } = req.body;
   const { folder } = req.params;
 
   const findFolder = await Folder.findOne({
     _id: folder,
-    createdBy: user._id,
+    createdBy: userId,
   });
 
   if (!findFolder) {
@@ -84,8 +95,9 @@ export const renameFolder = asyncHandler(async (req, res) => {
     { $set: { folderName: name } },
     { new: true },
   );
+
   if (!updatedFolder) {
-    throw new ApiError(404, "Folder not renamed");
+    throw new ApiError(500, "Folder not renamed");
   }
 
   await redisClient.del(`folder:${userId}`);
@@ -96,43 +108,38 @@ export const allFolders = asyncHandler(async (req, res) => {
   const user = req.user;
 
   if (!user) {
-    throw new ApiError(400, "User not found.");
+    throw new ApiError(401, "Not logged in");
   }
-  const userId = req.user._id;
+
+  const userId = user._id;
   const cachedKey = `folder:${userId}`;
   let cachedFolder;
+
   try {
     cachedFolder = await redisClient.get(cachedKey);
   } catch (err) {
     console.log("Redis error, fallback to DB");
   }
 
-  console.log("**********", cachedFolder);
-
   if (cachedFolder) {
-    console.log("cached here");
     return res
       .status(200)
-      .json(new ApiResponse(200, JSON.parse(cachedFolder), "FOLDERS Cache"));
+      .json(new ApiResponse(200, JSON.parse(cachedFolder), "Folders cached"));
   }
 
-  const allFolders = await Folder.find({ createdBy: user._id });
+  const allFolders = await Folder.find({ createdBy: userId });
 
   if (!allFolders) {
-    throw new ApiError(400, "Folders not found");
+    throw new ApiError(404, "Folders not found");
   }
 
   const cleanFolder = allFolders.map((folder) => folder.toObject());
-
-  // console.log(allFolders[0].constructor.name); // "model"
-
-  // console.log(cleanFolder[0].constructor.name); // "object"
 
   try {
     await redisClient.setEx(cachedKey, 60, JSON.stringify(cleanFolder));
   } catch (err) {
     console.log("Redis set failed");
   }
-  // console.log("FOLDERS: ", allFolders);
-  return res.status(200).json(new ApiResponse(200, cleanFolder, "FOLDERS"));
+
+  return res.status(200).json(new ApiResponse(200, cleanFolder, "Folders"));
 });
